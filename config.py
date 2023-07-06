eee6d188e14809725c89d0f0f84a367bf62f8438
邱堂珂 <q_qiutangke@163.com>
2023-07-06 07:50:51 +0000

    update config.py.
    Signed-off-by: 邱堂珂 <q_qiutangke@163.com>

--- config.py
+++ config.py
@@ -11,8 +11,12 @@ OBS_BUCKET_NAME = ""
 CACHE_DIR = r"/tmp/.cache"
 
 # Open Proxy Rules
-OPEN_PROXY_RULES = False
-SUFFIX_RULES = ".tar .gz .zip .xz .bz2 .tgz"
+OPEN_PROXY_RULES = True
+SUFFIX = ['sign', 'tgz', 'tbz', 'mim', '0', 'pom', 'xpi', 'lzma', 'tex', 'ls', 'wget', 'xsd', 'root', 'java', 'gpg',
+          'gz', 'bz2', 'el', 'md', 'rb', 'pdf', 'sha256sum', 'ml', "bz2'", 'xz', 'jar', 'txz', 'gem', 'ttf', 'oxt',
+          'json', 'tbz2', 'sh', 'awk', 'pem', 'zip', 'asc', 'dat', 'py', 'properties', 'distribution-zip', 'js', 'c',
+          'rpm', '7z', 'txt', 'html', 'sig', 'dtd', 'pod', 'whl', 'xml']
+URL_WHITE_LIST = []
 
 # proxy
 PROXY_URL = ""
